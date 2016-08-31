a791816b8eb0121b7e152338fd3fc41c080ba90b
Cary <guojiashuang@live.com>
2016-08-31 14:46:29 +0800

    界面优化

--- JSPopView/JSPopViewController.m
+++ JSPopView/JSPopViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "JSPopViewController.h"
+#import "JSPopView.h"
+
+#define js_width [[UIScreen mainScreen] bounds].size.width
+#define js_height [[UIScreen mainScreen] bounds].size.height
 
 @interface JSPopViewController ()
-
+@property(nonatomic, strong)NSMutableArray * dataArray;
 @end
 
 @implementation JSPopViewController
@@ -17,11 +21,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.dataArray = [NSMutableArray arrayWithObjects:@"🍊橘子",@"🍎苹果",@"🍌香蕉",@"🍉西瓜",@"🍈甜瓜",@"🍅西红柿",@"🍊橘子",@"🍎苹果",@"🍌香蕉",@"🍉西瓜",@"🍈甜瓜",@"🍅西红柿", nil];
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 64, js_width, js_height-64) style:UITableViewStylePlain];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,68 +33,40 @@
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return self.dataArray.count;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 80;
 }
 
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    cell.textLabel.text = self.dataArray[indexPath.row];
+    cell.backgroundColor = [UIColor grayColor];
     return cell;
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    UITableViewCell * cell = [self.tableView cellForRowAtIndexPath:indexPath];
+    CGRect rect = [cell.superview convertRect:cell.frame toView:[UIApplication sharedApplication].keyWindow];
+    CGFloat x = rect.size.width - 180;
+    CGFloat y = rect.origin.y;
+    if (y<64) {
+        y=64;
+    }
+    y+=10;
+    JSPopModel * model1 = [[JSPopModel alloc] initWithTitle:@"🍊橘子" imageName:nil];
+    JSPopModel * model2 = [[JSPopModel alloc] initWithTitle:@"🍎苹果" imageName:nil];
+    JSPopModel * model3 = [[JSPopModel alloc] initWithTitle:@"🍌香蕉" imageName:nil];
+    JSPopModel * model4 = [[JSPopModel alloc] initWithTitle:@"🍉西瓜" imageName:nil];
+    JSPopModel * model5 = [[JSPopModel alloc] initWithTitle:@"🍈甜瓜" imageName:nil];
+    JSPopModel * model6 = [[JSPopModel alloc] initWithTitle:@"🍅西红柿" imageName:nil];
+    NSArray * dataArray = @[model1,model2,model3,model4,model5,model6];
+    JSPopView * popView = [[JSPopView alloc] initWithDataArray:dataArray origin:CGPointMake(x, y) width:150 height:40 direction:JSPopViewDirectionLeft];
+    [popView pop];
 }
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
