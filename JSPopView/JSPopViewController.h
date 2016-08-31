315b16dc07302a636b435682de279b271071c581
Cary <guojiashuang@live.com>
2016-08-31 14:35:50 +0800

    界面修改

---
+++ JSPopView/JSPopViewController.h
@@ -0,0 +1,13 @@
+//
+//  JSPopViewController.h
+//  JSPopView
+//
+//  Created by 冯伟 on 16/8/31.
+//  Copyright © 2016年 Cary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSPopViewController : UITableViewController
+
+@end
--- JSPopView/ViewController.m
+++ JSPopView/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "JSPopView.h"
+#import "JSPopViewController.h"
 
 @interface ViewController ()
 
@@ -19,14 +20,27 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-- (IBAction)clicked:(id)sender {
-    JSPopModel * model1 = [[JSPopModel alloc] initWithTitle:@"首页" imageName:nil];
-    JSPopModel * model2 = [[JSPopModel alloc] initWithTitle:@"第二页" imageName:nil];
-    JSPopModel * model3 = [[JSPopModel alloc] initWithTitle:@"第三页" imageName:nil];
-    JSPopModel * model4 = [[JSPopModel alloc] initWithTitle:@"第四页" imageName:nil];
+- (IBAction)left:(id)sender {
+    JSPopModel * model1 = [[JSPopModel alloc] initWithTitle:@"🍊橘子" imageName:nil];
+    JSPopModel * model2 = [[JSPopModel alloc] initWithTitle:@"🍎苹果" imageName:nil];
+    JSPopModel * model3 = [[JSPopModel alloc] initWithTitle:@"🍌香蕉" imageName:nil];
+    JSPopModel * model4 = [[JSPopModel alloc] initWithTitle:@"🍉西瓜" imageName:nil];
     NSArray * dataArray = @[model1,model2,model3,model4];
-    JSPopView * popView = [[JSPopView alloc] initWithDataArray:dataArray origin:CGPointMake(100, 200) width:100 height:40 direction:JSPopViewDirectionLeft];
-    [popView popInView:self.view];
+    JSPopView * popView = [[JSPopView alloc] initWithDataArray:dataArray origin:CGPointMake(20, 64) width:100 height:40 direction:JSPopViewDirectionLeft];
+    [popView pop];
+}
+- (IBAction)right:(id)sender {
+    JSPopModel * model1 = [[JSPopModel alloc] initWithTitle:@"🍊橘子" imageName:nil];
+    JSPopModel * model2 = [[JSPopModel alloc] initWithTitle:@"🍎苹果" imageName:nil];
+    JSPopModel * model3 = [[JSPopModel alloc] initWithTitle:@"🍌香蕉" imageName:nil];
+    JSPopModel * model4 = [[JSPopModel alloc] initWithTitle:@"🍉西瓜" imageName:nil];
+    NSArray * dataArray = @[model1,model2,model3,model4];
+    JSPopView * popView = [[JSPopView alloc] initWithDataArray:dataArray origin:CGPointMake(self.view.frame.size.width - 20, 64) width:100 height:40 direction:JSPopViewDirectionRight];
+    [popView pop];
+}
+- (IBAction)clicked:(id)sender {
+    JSPopViewController *vc = [[JSPopViewController alloc] init];
+    [self.navigationController pushViewController:vc animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
